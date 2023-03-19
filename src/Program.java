f337233619cdb178f8b520511616f9843a24b593
Marlon Müller <marlon130403@gmail.com>
2023-03-19 15:56:48 +0100

    Setting up the project

---
+++ src/Program.java
@@ -0,0 +1,52 @@
+package src;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Program {
+    String key, clientID;
+    Scanner in;
+    public static void main(String[] args) {
+        Program program = new Program();
+    }
+
+    Program() {
+        in = new Scanner(System.in);
+        boolean running = true;
+        try {
+            key = Files.readString(Path.of("key.txt"));
+            clientID = Files.readString(Path.of("clientID.txt"));
+        } catch (IOException e) {
+            System.err.println("No API credentials were found. No connection to spotify services can be established");
+        }
+
+        while (running) {
+            int input = -1;
+            do {
+                System.out.println("Wähle eine Option:");
+                System.out.println("(0) Shutdown   (1) Filter new Playlist");
+                try {
+                    input = in.nextInt();
+                } catch (InputMismatchException ignored) {
+
+                }
+            } while (input < 0 || input > 1);
+
+            switch (input) {
+                case 0 -> running = false;
+                case 1 -> {
+                    System.out.println("Enter playlist ID:");
+                    String id = in.next();
+                    filterDuplicates(id);
+                }
+            }
+        }
+    }
+
+    public void filterDuplicates(String playlistID) {
+
+    }
+}
